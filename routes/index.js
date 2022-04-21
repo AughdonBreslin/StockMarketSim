b439852fb48c94fdd70f6a30dd6025bd4378257c
AughdonBreslin <augh.breslin@gmail.com>
2022-04-21 15:14:26 -0400

    made a bunch of handlebars/organized routing a bit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 
-const userApiRoutes = require('./userApi');
-
 const constructorMethod = (app) => {
+    // Home Page
+    app.get('/', (req, res) => {
+        if(req.session.username) {
+            res.render('homePages/home.handlebars', {title: 'My Market Simulator'});
+        } else {
+            res.render('account/login.handlebars', {title: "Login"});
+        }
+    });
 
-    app.use('/', userApiRoutes);
+    // Login and Signup Page
+    app.get('/login', (req, res) => {
+        res.render('account/login.handlebars', {title: "Login"});
+    })
+    app.get('/signup', (req, res) => {
+        res.render('account/signup.handlebars', {title: "Sign Up"});
+    })
+    // Trade Page
+
+    // View Portfolio Page
+
+    // etc
 
     app.use('*', (req, res) => {
         res.status(404).json({ error: 'Not found' });
---
+++ routes/trade.js
@@ -0,0 +1,11 @@
+
+// const express = require('express');
+// const router = express.Router();
+
+// // const data = require('../data');
+// // const userApiData = data.userApi;
+
+
+
+
+// module.exports = router;
