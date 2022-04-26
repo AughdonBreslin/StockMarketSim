6b3975eefd01310dfda72301874aa839f9514e8b
Ankit Patel <ankit.patel.1701.5467@gmail.com>
2022-04-25 23:51:14 -0400

    Added DepositHistory functions

--- data/depositHist.js
+++ data/depositHist.js
@@ -1,3 +1,5 @@
+// Document format example:
+
 // {
 //     "_id": "507f1f77bcf86cd799439011",
 
@@ -9,7 +11,7 @@
 // }
 
 const mongoCollections = require('../config/mongoCollections');
-const users = mongoCollections.users;
+const deposits = mongoCollections.deposits;
 const {ObjectId} = require('mongodb');
 const validation = require('../validation');
 
@@ -20,15 +22,22 @@ const getDepositsOfUser = async function(stockPortfolio_id) {
     // error checking
     validation.checkNumOfArgs(arguments,1,1);
 
+    // Check if stockPortfolio_id is valid input
+    if (!ObjectId.isValid(stockPortfolio_id)) throw `Error: Invalid stock portfolio ID provided.`;
 
-    // make connection and get collection
+    // get collection
+    const depositsCollection = await deposits();
+    if(!depositsCollection) throw `Error: Could not find depositsCollection.`;
 
-    // MongoDB Query
+    // MongoDB Query -- get all depositHist Id's associated w/ the stockPortfolio_id
+    const stkport_deps = await depositsCollection.find({stockPortfolio_id: stockPortfolio_id}, {_id: 1});
+    
+    // Check if query returned a valid value    
+    // If query returns [] (ie: no deposits associated w/ a stockPortfolio_id, then error bc this should not happen) else, return an array of depositHistory_ids
 
-    // Check if query returned a valid value
+    if(stkport_deps.toArray().length === 0) throw `Error: The stock portfolio has no deposits associated with it. This should NOT happen!`;
 
-    // If returned value = [] (ie: no deposits associated w/ a stockPortfolio_id, then error bc this should not happen) else, return an array of depositHistory_ids
-
+    return stkport_deps.toArray();
 
 }
 
@@ -37,41 +46,53 @@ const getDeposit = async function(deposit_id) {
     // error checking
     validation.checkNumOfArgs(arguments,1,1);
 
+    // Check if deposit_id is valid input
+    if (!ObjectId.isValid(deposit_id)) throw `Error: Invalid deposit ID provided.`;
 
-    // make connection and get collection
+    // get collection
+    const depositsCollection = await deposits();
+    if(!depositsCollection) throw `Error: Could not find depositsCollection.`;
 
-    // mongoDB query
+    // MongoDB Query -- get deposit info associated with the given id
+    const deposit = await depositsCollection.findOne({_id: deposit_id});
 
     // check if query returned a valid value. If it did, return the item. else error. 
+    if (Object.keys(deposit).length === 0) throw `Error: A deposit with the given ID was NOT found. This should NOT happen!`;
+    
+    return deposit;
 
 }
 
 
 /** add a dep hist elem for a user (fires when $ is deposited into a user's account)
-Note: The function adds only a record of a deposit. It does not change the actual account balance. To change the account balance (stockPortfolio.current-balance), a seperate function should be called in the stockPortfolio subdoc. */
-const addDepositRecord = async function(stockPortfolio_id, dep_amnt, man_or_aut, dep_date) {
+Note: The function adds only a record of a deposit. It does not change the actual account balance. To change the account balance (stockPortfolio.current-balance), a seperate function should be called in the stockPortfolio subdoc. Return the added deposit record. */
+const addDepositRecord = async function(stockPortfolio_id, dep_amnt, man_or_aut) {
     // error checking
-    validation.checkNumOfArgs(arguments,4,4);
+    validation.checkNumOfArgs(arguments,3,3);
+    if (!ObjectId.isValid(stockPortfolio_id)) throw `Error: Invalid stock portfolio ID provided.`;
     validation.checkInt(dep_amnt, "Deposit Amount");
-    // validation. // check function that checks if boolean
-    // 
+    validation.checkBoolean(man_or_aut, "Manual or Automated");
 
     // Make the depositRecord Object
     const depRecObj = {
-        "_id": ObjectId(),
+        // "_id": ObjectId(),
         "stockPortfolio_id": stockPortfolio_id,
         "deposit-amount": dep_amnt,
         "manual-or-automated": man_or_aut,
-        "deposit-date": dep_date
-    }
+        "deposit-date": new Date()
+    };
 
-    // Make connection and get collection
+    // get collection
+    const depositsCollection = await deposits();
+    if(!depositsCollection) throw `Error: Could not find depositsCollection.`;
 
-    // Add the Object to the collection
+    // MongoDB Query -- get deposit info associated with the given id
+    const newDeposit = await depositsCollection.insertOne(depRecObj);
 
-    // Check the above return value and return the proper return value
-
-
+    // check if query returned the newly created value. If it did, return the item. else error. 
+    if (Object.keys(newDeposit).length === 0) throw `Error: A${man_or_aut ? " manual" : "n automated" } $${dep_amnt} deposit could not be created for stock portfolio ${stockPortfolio_id}. This should NOT happen!`;
+    
+    return newDeposit;
 
 }
 
