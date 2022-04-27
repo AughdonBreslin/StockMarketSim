2a3eaec80ee6a1cacc31a56c137525facc92378e
EllaCrabtree <ellajeantree5302@gmail.com>
2022-04-26 20:31:31 -0400

    Template for stockPortfolio.js + updated jsonc to reflect changes to schema

--- data/stockPortfolio.js
+++ data/stockPortfolio.js
@@ -9,11 +9,42 @@ const createPortfolio = async function createPortfolio() {
 
 }
 
+//Updates portfolio-value field with the current stock portfolio value
+//Adds old PVal to PValHistory array
+const updatePVal = async function updatePVal(pVal) {
 
+}
+
+//Updates current-balance field with the current stock 
+const updateCurrentBal = async function updateCurrentBal(currBal) {
+
+}
+
+//Adds despoitHistory document id to array in stockPortfolio
+const addToDepHist = async function addToDepHist() {
+
+}
+
+//Adds AutomatedPurchase document id to array in stockPortfolio document
+const addToAutoPurchases = async function addToAutoPurchases() {
+
+}
+
+//Adds AutomatedSelling document id to array in stockPortfolio document
+const addToAutoSell = async function addToAutoSell() {
+
+}
+
+const addToTransactionLog = async function addToTransactionLog() {
+
+}
 
 module.exports = {
-
-
-
-
+    createPortfolio,
+    updatePVal,
+    updateCurrentBal,
+    addToDepHist,
+    addToAutoPurchases,
+    addToAutoSell,
+    addToTransactionLog
 }
