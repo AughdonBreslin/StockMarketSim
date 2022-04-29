62d48e417dd9b6cf1161d909d5cc5e0f3b281155
Ankit Patel <ankit.patel.1701.5467@gmail.com>
2022-04-28 22:26:58 -0400

    Tried implementing a trade route

--- data/index.js
+++ data/index.js
@@ -2,11 +2,13 @@ const userData = require('./users');
 const depHistData  = require('./depositHist');
 const stockSet = require('./stockSettings.js');
 const stockPort = require('./stockPortfolio.js');
+const transactionData = require('./transactions.js');
 
 // not much to do here
 module.exports = {
   users: userData,
   depHist: depHistData,
   stockSettings: stockSet,
-  stockPortfolio: stockPort
+  stockPortfolio: stockPort,
+  transactions: transactionsData
 };
