a949777923db3cc1d6dfe54f9e6eedd9405732f6
Ankit5467 <ankit.patel.1701.5467@gmail.com>
2022-05-06 20:49:25 -0400

    Trying to implement positions
    - templating isnt working for some reason

--- routes/homePages/positions.js
+++ routes/homePages/positions.js
@@ -6,12 +6,40 @@ const userData = data.users;
 
 // GET /positions
 router.get('/', (req, res) => {
-    if(req.session.username) {
-        res.render('homePages/positions.handlebars',
-            {title: 'Positions', user: 'TODO'});
-        } else {
-            res.redirect('/login');
-        }
+    if (req.session.username) {
+
+        /* Get user_id from their username */
+        /* username must be unique */
+        const userId = await data.users.getUserIdFromUsername(req.session.username);
+
+        // const stk_prt = await data.stockPortfolio.getSP(userId);
+
+        // For testing:
+        // const stk_prt = {
+        //     stock: [ /* store stocks as nested array */
+        //         ["GOOG", 10], /* min: 0. max: Number.MAX_SAFE_INTEGER - 1 */
+        //         ["TSLA", 5],
+        //         ["AAPL", 6],
+        //         ["AMZN", 100]
+        //     ]
+        // };
+
+        const stocks = [ /* store stocks as nested array */
+            ["GOOG", 10], /* min: 0. max: Number.MAX_SAFE_INTEGER - 1 */
+            ["TSLA", 5],
+            ["AAPL", 6],
+            ["AMZN", 100]
+        ];
+
+        res.render('../views/homePages/positions.handlebars',
+            {
+                title: 'Positions', user: 'TODO', str: 'hello', stocks: stocks, myList: ["a", "f", "g"]
+            }
+        );
+
+    } else {
+        res.redirect('/login');
+    }
 });
 
 module.exports = router;
