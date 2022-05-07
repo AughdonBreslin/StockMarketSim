b204ee8655819119abaf98a1d53ca52866ae4c9a
Ankit5467 <ankit.patel.1701.5467@gmail.com>
2022-05-06 21:45:37 -0400

    Updated positions.js
    positions.js now shows the list of positions

--- routes/homePages/positions.js
+++ routes/homePages/positions.js
@@ -5,25 +5,18 @@ const data = require('../../data');
 const userData = data.users;
 
 // GET /positions
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     if (req.session.username) {
 
         /* Get user_id from their username */
         /* username must be unique */
         const userId = await data.users.getUserIdFromUsername(req.session.username);
 
+        // uncomment this 
         // const stk_prt = await data.stockPortfolio.getSP(userId);
+        // const stocks = stk_prt.stocks;
 
-        // For testing:
-        // const stk_prt = {
-        //     stock: [ /* store stocks as nested array */
-        //         ["GOOG", 10], /* min: 0. max: Number.MAX_SAFE_INTEGER - 1 */
-        //         ["TSLA", 5],
-        //         ["AAPL", 6],
-        //         ["AMZN", 100]
-        //     ]
-        // };
-
+        // For testing comment this later:
         const stocks = [ /* store stocks as nested array */
             ["GOOG", 10], /* min: 0. max: Number.MAX_SAFE_INTEGER - 1 */
             ["TSLA", 5],
@@ -33,7 +26,7 @@ router.get('/', (req, res) => {
 
         res.render('../views/homePages/positions.handlebars',
             {
-                title: 'Positions', user: 'TODO', str: 'hello', stocks: stocks, myList: ["a", "f", "g"]
+                title: 'Positions', user: 'TODO', stocks: stocks
             }
         );
 
