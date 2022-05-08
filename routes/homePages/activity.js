fa5b2a14f847f852b820d47d2fa3bad70991b7ba
AughdonBreslin <augh.breslin@gmail.com>
2022-05-08 19:08:12 -0400

    activity tweaks

--- routes/homePages/activity.js
+++ routes/homePages/activity.js
@@ -8,50 +8,29 @@ const transactions = require('../../data/transactions');
 // GET /activity
 router.get('/', async (req, res) => {
     if (req.session.username && req.session.stockPortId) {
-
         try {
             /* Get user_id from their username */
             /* username must be unique */
             let userID = await users.getUserIdFromUsername(req.session.username);
             userID = userID.toString();
-            console.log(`User ${userID} went to home page.`);
+            console.log(`User ${userID} went to activity page.`);
             let portfolio = await stockPortfolio.getSP(req.session.stockPortId, userID);
             let depHist = portfolio.depositHistory;
             let transHist = portfolio.transactions;
             let autoTradeHist = portfolio.awaitingTrades;
 
-            // Get a list of the automated purchase/sell orders:
-            // get awaitingTrades list
-            // const automated_orders = await data.
-            // const automated_orders = [
-            //     {
-            //         "_id": "507f1f77bcf86cd799439011",
-            //         "type": "buy", // Possible Values: ["buy", "sell"]
-            //         "ticker": "GOOG",
-            //         "quantity": 10,
-            //         "pps_threshold": 50.42,
-            //         "priority": 4 // Any positive number > 0
-            //     },
-            //     {
-            //         "_id": "507f1f77bcf86cd799439011",
-            //         "type": "sell", // Possible Values: ["buy", "sell"]
-            //         "ticker": "GOOG",
-            //         "quantity": 10,
-            //         "pps_threshold": 50.42,
-            //         "priority": -1 // Should always be -1 for sells
-            //     }
-            // ];
-            // const automated_orders = [];
-
-            res.render('homePages/activity.handlebars',
-                { title: 'Activity', user: 'TODO', transactions: transHist, deposits: depHist, awaitingTrades: autoTradeHist }
-
-            );
+            res.render('homePages/activity.handlebars', {
+                title: 'Activity',
+                transactions: transHist,
+                deposits: depHist,
+                awaitingTrades: autoTradeHist
+            });
 
         } catch (error) {
             res.render('additional/error.handlebars',
                 {
-                    title: 'Error', errorMessage: error.toString()
+                    title: 'Error',
+                    errorMessage: error.toString()
                 }
             );
         }
