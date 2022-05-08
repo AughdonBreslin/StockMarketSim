25607f3a68f7682077a40116437fa0037e908115
Christian Bautista <christiangbautista@gmail.com>
2022-05-08 19:14:04 -0400

    Finished debugging automatic buy/sell

--- test.js
+++ test.js
@@ -14,8 +14,19 @@ async function test() {
     // } catch(e) {
     //     console.log(e)
     // }
+
+    // try {
+    //     const result = await transactions.buy("6278298ba1eb25c069188944", "GOOG", 1, false, 2500, 1, true)
+    // } catch(e) {
+    //     console.log(e)
+    // }
+    // try {
+    //     const result = await transactions.buy("6278298ba1eb25c069188944", "AMZN", 1, false, 2500, 1, true)
+    // } catch(e) {
+    //     console.log(e)
+    // }
     try {
-        const result = await transactions.buy("6278298ba1eb25c069188944", "GOOG", 1, false, 2500, 10, true)
+        const result = await transactions.sell("6278298ba1eb25c069188944", "GOOG", 1, false, 5000, true)
     } catch(e) {
         console.log(e)
     }
