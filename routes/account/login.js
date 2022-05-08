bfe5b3adb8f11aecb29aaff70e9df66dc5361e5b
EllaCrabtree <ellajeantree5302@gmail.com>
2022-05-08 19:17:49 -0400

    Added auto depo functionality

--- routes/account/login.js
+++ routes/account/login.js
@@ -43,6 +43,7 @@ router.post('/', async (req, res) => {
         return res.redirect('/createPortfolio');
       } else {
         req.session.stockPortId = stockPort._id;
+        await stockPortData.setAutoDeposit(stockPort._id.toString(), status.user._id.toString());
       }
       
     } catch (e) {
